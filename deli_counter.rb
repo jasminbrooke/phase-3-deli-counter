0570cc041ff142d4df3ffb2d827a1e54e0a439c8
+ <+>
2022-10-23 20:26:51 -0400

    commit tree e0d4cd81c3f349e0924ed7043e65d70b0ff6327d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(customers)
+    if customers.empty?
+        puts "The line is currently empty."
+    else
+        line = ''
+        customers.each_with_index{ |c, index| line += "#{index + 1}. #{c} " }
+        puts "The line is currently: #{line.strip}"
+    end
+end
+
+def take_a_number(customers, person)
+    customers.push(person)
+    puts "Welcome, #{person}. You are number #{customers.length} in line."
+end
+
+def now_serving(customers)
+    if customers.empty?
+       puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{customers[0]}."
+        customers.shift
+    end
+end
